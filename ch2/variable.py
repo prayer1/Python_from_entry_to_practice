6e9fefa065f6520cd4fb9f2a1f8538e92f48254a
prayer1 <274611210@qq.com>
2020-01-31 00:09:21 +0800

    ch2

---
+++ ch2/variable.py
@@ -0,0 +1,20 @@
+name = "ada loveace"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+first_name = "zhu"
+last_name = "shengquan"
+full_name = first_name + " " +last_name
+print("Hello, " + full_name + "!")
+
+favorite_language = "python "
+print(favorite_language + "1")
+print(favorite_language.rstrip() + "1")
+
+# str()
+age = 20
+message = "Happy " + str(age) + "rd Birthday!"
+print(message);
+
+import this
